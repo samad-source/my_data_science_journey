c8ab8d3068224cdaef4c307785488610120a4602
unscripted <s.azeez8553@miva.edu.ng>
2026-09-08 09:07:58 +0100

    control flow

---
+++ control_flow.py
@@ -0,0 +1,7 @@
+# Do multiplication table 7 with for loop
+for i in range(1,13):
+    print(f"7 x {i} = {7*i}")
+    
+# print a left aligned pyramid of stars with 6 rows using for loop
+for i in range(1,7):
+    print("*" * i)
