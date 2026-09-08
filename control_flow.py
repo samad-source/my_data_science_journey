28729fd21a63460b026dea48dbc830867920bebd
unscripted <s.azeez8553@miva.edu.ng>
2026-09-08 09:33:23 +0100

    control_flow

--- control_flow.py
+++ control_flow.py
@@ -5,3 +5,11 @@ for i in range(1,13):
 # print a left aligned pyramid of stars with 6 rows using for loop
 for i in range(1,7):
     print("*" * i)
+    
+# Skip weekends in a calendar using for loop
+days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+for day in days:
+    if day == "Saturday" or day == "Sunday":
+        print(f"Today is {day} and it is a weekend day")
+        continue
+    print(f"Today is {day} and it is a working day")
