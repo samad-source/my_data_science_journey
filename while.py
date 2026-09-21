d8a76b504379d0e3ee85295842621de2564f01c1
samad-source <s.azeez8553@miva.edu.ng>
2026-09-21 11:43:54 +0100

    while loops

--- while.py
+++ while.py
@@ -1,4 +1,18 @@
-number = 100
-while 1 < number :
-    print("active")
-    number -= 1
+# number = 100
+# while 1 < number :
+#     print("active")
+#     number -= 1
+
+counter = 3
+while True:
+    answer = input("Did you love me ? (yes/no): ")
+    if answer == "yes" :
+        print("I LOVE YOU TOO !!!!")
+        break      
+    elif answer != "yes":
+        counter -= 1
+        print(f"you have {counter} attempt left")
+        if counter == 0:
+            print("Time Out")
+            break
+        
