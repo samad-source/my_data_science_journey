44ebb37584179abcf52ef93755a465b2947f624f
samad-source <s.azeez8553@miva.edu.ng>
2026-09-26 01:28:20 +0100

    function

--- function.py
+++ function.py
@@ -12,7 +12,8 @@ def calculate_average(students):
     for student in students:
         total += student["score"]
         average = total / len(students)
-        return average
+
+    return average
 
 result = calculate_average(students)
 print(result)
@@ -23,7 +24,8 @@ def count_passed(students):
         for student in students:
             if student["score"] >= 50:
                 passed += 1
-                return passed
+
+        return passed
 
 results = count_passed(students)
 print(results)
@@ -31,11 +33,11 @@ print(results)
 # function 3
 def get_excellent_score(students):
     excellent = []
-    passed = 0
-    failed = 0
+
     for student in students:
-        if student["score"] >= 75:
-            excellent = excellent.append(student["name"])
-            return excellent 
+        if student["score"] >= 70:
+            excellent.append(student["name"])
+
+    return excellent 
 grade = get_excellent_score(students)
 print(grade)
