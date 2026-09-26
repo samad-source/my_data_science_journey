1dfa5465d0c4b0b8959a3c81780b2a8a28251739
samad-source <s.azeez8553@miva.edu.ng>
2026-09-26 13:37:24 +0100

    OOP

---
+++ OOP.py
@@ -0,0 +1,7 @@
+class person:
+    def __init__(self,name,age,address):
+        self.name = name = input("enter name")
+        self.age = age
+        self.address = address
+name1 = person(name,age,address)
+print(name1.name)
